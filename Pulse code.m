63f256ac839c5cb6cd9af2605c5acaa15306c22b
2k232 <138819691+2k232@users.noreply.github.com>
2023-07-07 01:50:20 +0530

    Create Pulse code.m

---
+++ Pulse code.m
@@ -0,0 +1,20 @@
+clc;
+close all; 
+clear all;
+t = 0:0.0001:20; %sampling at niquist 
+c=input ('Enter Bit Depth of PCM Coding:');
+part = -1:0.1:1;	%A quantization partition defines several contiguous, nonoverlapping ranges of values within the set of real numbers.
+codebook = -1:0.1:1.1; %A codebook tells the quantizer which common value to assign to inputs that fall into each range of the partition.
+msg = cos(t);
+[index,quants] = quantiz(msg,part,codebook); %returns a vector that tells which interval each input is in subplot(3,1,1);
+subplot(3,1,1);
+plot(t,msg);
+title('Message Signal');
+subplot(3,1,2); 
+plot(t,quants);
+title('Quantized Signal'); 
+y = uencode(quants,c);
+ybin=dec2bin(y,c); %converting it to final binary form to make it transmit ready
+subplot(3,1,3);
+plot(t,y);
+title('PCM PLOT');
