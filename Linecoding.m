fc1c0f63ac4a58ff59c8cb34598510a2639b00ed
2k232 <138819691+2k232@users.noreply.github.com>
2023-07-07 01:44:19 +0530

    Create Linecoding.m

---
+++ Linecoding.m
@@ -0,0 +1,11 @@
+clc;
+clear all;
+close all;
+t=0:0.01:(2*pi);
+x=cos(t);
+y=sin(t);
+plot(t,x,'b');
+hold on;
+plot(t,y,'r');
+axis([0 2*pi -1.5 1.5]);
+legend('cos(t)','sin(t)','Location','NorthEast');
