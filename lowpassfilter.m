4a29a6f613cd72d6ec08ed8c8e846e50b317937f
2k232 <138819691+2k232@users.noreply.github.com>
2023-07-07 02:06:05 +0530

    Delete lowpassfilter.m

--- lowpassfilter.m
+++
@@ -1,8 +0,0 @@
-clc;
-clear all;
-close all;
-fs=input('enter the sampling frequency(in hz)');
-%44100hz
-fp=input('enter the pass band frequency(inhz)');
-tw=input('enter the transition width(in hz)');as
-
